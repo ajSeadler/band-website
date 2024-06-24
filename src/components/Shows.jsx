894a015e29edb7c13ff1d611b20408cd3dd559f4
ajSeadler <aj6287@gmail.com>
2024-06-24 15:31:33 -0500

    add show modal

--- src/components/Shows.jsx
+++ src/components/Shows.jsx
@@ -2,17 +2,14 @@ import React, { useState, useEffect } from "react";
 import {
   Paper,
   Typography,
-  Grid,
   Card,
   CardContent,
   Dialog,
   DialogContent,
-  Button,
   IconButton,
 } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import { ClipLoader } from "react-spinners";
-import { shows, pastShows } from "./showinfo";
 import EventIcon from "@mui/icons-material/Event";
 import ScheduleIcon from "@mui/icons-material/Schedule";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
@@ -22,6 +19,9 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/Shows.css";
 
+// Importing shows and pastShows from showinfo.js
+import { shows, pastShows } from "./showinfo";
+
 const Shows = () => {
   const [open, setOpen] = useState(false);
   const [selectedShow, setSelectedShow] = useState(null);
@@ -30,7 +30,7 @@ const Shows = () => {
   useEffect(() => {
     const timer = setTimeout(() => {
       setLoading(false);
-    }); // Set a delay for the loading spinner
+    }); // Simulating loading for 1 second
 
     return () => clearTimeout(timer);
   }, []);
@@ -78,149 +78,175 @@ const Shows = () => {
   };
 
   return (
-    <>
-      <div className="shows-pg">
-        {loading && (
-          <div className="loader-overlay">
-            <ClipLoader color="#36D7B7" loading={loading} size={50} />
+    <div className="shows-pg">
+      {loading && (
+        <div className="loader-overlay">
+          <ClipLoader color="#36D7B7" loading={loading} size={50} />
+        </div>
+      )}
+
+      {!loading && (
+        <Paper elevation={0} className="shows-pg">
+          <div className="custom-font-shows">
+            <h1>UPCOMING SHOWS</h1>
           </div>
-        )}
-
-        {!loading && (
-          <Paper elevation={0} className="shows-pg">
-            {/* <div className="custom-font-shows">
-              <h1>UPCOMING SHOWS</h1>
-            </div> */}
-            <Slider {...sliderSettings} className="shows-carousel">
-              {shows.map((show, index) => (
-                <div key={index}>
-                  <Card
-                    className="show-card"
-                    onClick={() => handleCardClick(show)}
-                  >
-                    <img
-                      src={show.image}
-                      alt={`Band Picture ${index}`}
-                      className="show-image"
-                    />
-                    <CardContent className="show-card-content">
-                      <h6
-                        style={{
-                          fontSize: "1.3rem",
-                          textDecoration: "underline",
+          <Slider {...sliderSettings} className="shows-carousel">
+            {shows.map((show, index) => (
+              <div key={index}>
+                <Card
+                  className="show-card"
+                  onClick={() => handleCardClick(show)}
+                >
+                  <img
+                    src={show.image}
+                    alt={`Band Picture ${index}`}
+                    className="show-image"
+                  />
+                  <CardContent className="show-card-content">
+                    <h6
+                      style={{
+                        fontSize: "1.3rem",
+                        textDecoration: "underline",
+                      }}
+                    >
+                      {show.title}
+                    </h6>
+                    <Typography>
+                      <EventIcon sx={{ color: "#ffbd01", marginRight: 1 }} />{" "}
+                      {show.date}
+                    </Typography>
+                    <Typography>
+                      <ScheduleIcon sx={{ color: "#4a90e2", marginRight: 1 }} />{" "}
+                      {show.time}
+                    </Typography>
+                    <Typography>
+                      <MonetizationOnIcon
+                        sx={{ color: "#1ed760", marginRight: 1 }}
+                      />{" "}
+                      {show.price}
+                    </Typography>
+                    <Typography variant="body2">
+                      <LocationOnIcon
+                        sx={{
+                          color: "#d34836",
+                          fontSize: "small",
+                          marginRight: 1,
                         }}
-                      >
-                        {show.title}
-                      </h6>
-                      <Typography>
-                        <EventIcon sx={{ color: "#ffbd01", marginRight: 1 }} />{" "}
-                        {show.date}
-                      </Typography>
-                      <Typography>
-                        <ScheduleIcon
-                          sx={{ color: "#4a90e2", marginRight: 1 }}
-                        />{" "}
-                        {show.time}
-                      </Typography>
+                      />{" "}
+                      {show.location}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </Slider>
+
+          <div className="custom-font-shows">
+            <h1 style={{ marginTop: "10%" }}>PAST SHOWS</h1>
+          </div>
+          <Slider {...sliderSettings} className="shows-carousel">
+            {pastShows.map((show, index) => (
+              <div key={index}>
+                <Card
+                  className="show-card"
+                  onClick={() => handleCardClick(show)}
+                >
+                  <img
+                    src={show.image}
+                    alt={`Band Picture ${index}`}
+                    className="show-image"
+                  />
+                  <CardContent className="show-card-content">
+                    <h6
+                      style={{
+                        fontSize: "1.3rem",
+                        textDecoration: "underline",
+                      }}
+                    >
+                      {show.title}
+                    </h6>
+                    <Typography>
+                      <EventIcon sx={{ color: "#ffbd01", marginRight: 1 }} />{" "}
+                      {show.date}
+                    </Typography>
+                    <Typography>
+                      <ScheduleIcon sx={{ color: "#4a90e2", marginRight: 1 }} />{" "}
+                      {show.time}
+                    </Typography>
+                    {show.price && (
                       <Typography>
                         <MonetizationOnIcon
                           sx={{ color: "#1ed760", marginRight: 1 }}
                         />{" "}
                         {show.price}
                       </Typography>
-                      <Typography variant="body2">
-                        <LocationOnIcon
-                          sx={{
-                            color: "#d34836",
-                            fontSize: "small",
-                            marginRight: 1,
-                          }}
-                        />{" "}
-                        {show.location}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              ))}
-            </Slider>
-
-            <div className="custom-font-shows">
-              <h1 style={{ marginTop: "10%" }}>PAST SHOWS</h1>
-            </div>
-
-            <Slider {...sliderSettings} className="shows-carousel">
-              {pastShows.map((show, index) => (
-                <div key={index}>
-                  <Card
-                    className="show-card"
-                    onClick={() => handleCardClick(show)}
-                  >
-                    <img
-                      src={show.image}
-                      alt={`Band Picture ${index}`}
-                      className="show-image-past"
-                    />
-                    <CardContent className="show-card-content">
-                      <h6
-                        style={{
-                          fontSize: "1.3rem",
-                          textDecoration: "underline",
+                    )}
+                    <Typography variant="body2">
+                      <LocationOnIcon
+                        sx={{
+                          color: "#d34836",
+                          fontSize: "small",
+                          marginRight: 1,
                         }}
-                      >
-                        {show.title}
-                      </h6>
-                      <Typography>
-                        <EventIcon sx={{ color: "#ffbd01", marginRight: 1 }} />{" "}
-                        {show.date}
-                      </Typography>
-                      <Typography>
-                        <ScheduleIcon
-                          sx={{ color: "#4a90e2", marginRight: 1 }}
-                        />{" "}
-                        {show.time}
-                      </Typography>
-                      <Typography>
-                        <MonetizationOnIcon
-                          sx={{ color: "#1ed760", marginRight: 1 }}
-                        />{" "}
-                        {show.price}
-                      </Typography>
-                      <Typography variant="body2">
-                        <LocationOnIcon
-                          sx={{
-                            color: "#d34836",
-                            fontSize: "small",
-                            marginRight: 1,
-                          }}
-                        />{" "}
-                        {show.location}
-                      </Typography>
-                    </CardContent>
-                  </Card>
-                </div>
-              ))}
-            </Slider>
-          </Paper>
-        )}
+                      />{" "}
+                      {show.location}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </div>
+            ))}
+          </Slider>
+        </Paper>
+      )}
 
-        <Dialog open={open} onClose={handleClose} className="full-image-dialog">
-          <DialogContent className="full-image-dialog-content">
-            <IconButton
-              aria-label="close"
-              onClick={handleClose}
-              className="close-button"
-            >
-              <CloseIcon />
-            </IconButton>
-            <img
-              src={selectedShow?.image}
-              alt={`Band Picture`}
-              className="show-image"
-            />
-          </DialogContent>
-        </Dialog>
-      </div>
-    </>
+      <Dialog open={open} onClose={handleClose} className="full-image-dialog">
+        <DialogContent className="full-image-dialog-content">
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            className="close-button"
+          >
+            <CloseIcon />
+          </IconButton>
+          {selectedShow && (
+            <Card className="show-card-modal">
+              <img
+                src={selectedShow.image}
+                alt={`Band Picture`}
+                className="show-image-modal"
+              />
+              <CardContent className="show-card-content">
+                <h6 style={{ fontSize: "1.3rem", textDecoration: "underline" }}>
+                  {selectedShow.title}
+                </h6>
+                <Typography>
+                  <EventIcon sx={{ color: "#ffbd01", marginRight: 1 }} />{" "}
+                  {selectedShow.date}
+                </Typography>
+                <Typography>
+                  <ScheduleIcon sx={{ color: "#4a90e2", marginRight: 1 }} />{" "}
+                  {selectedShow.time}
+                </Typography>
+                {selectedShow.price && (
+                  <Typography>
+                    <MonetizationOnIcon
+                      sx={{ color: "#1ed760", marginRight: 1 }}
+                    />{" "}
+                    {selectedShow.price}
+                  </Typography>
+                )}
+                <Typography variant="body2">
+                  <LocationOnIcon
+                    sx={{ color: "#d34836", fontSize: "small", marginRight: 1 }}
+                  />{" "}
+                  {selectedShow.location}
+                </Typography>
+              </CardContent>
+            </Card>
+          )}
+        </DialogContent>
+      </Dialog>
+    </div>
   );
 };
 

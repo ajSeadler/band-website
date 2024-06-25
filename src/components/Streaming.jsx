658f751c9e25735514d07129a40950361ae51a06
ajSeadler <aj6287@gmail.com>
2024-06-25 14:19:53 -0500

    round borders for most cards to 20px

--- src/components/Streaming.jsx
+++ src/components/Streaming.jsx
@@ -1,35 +1,16 @@
 import React from "react";
-import styled from "styled-components";
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
-`;
-
-const StyledIframe = styled.iframe`
-  border-radius: 0px;
-  margin-bottom: 10px;
-  margin: auto;
-  width: 90%;
-  height: 450px;
-  border: 0;
-  allowfullscreen: "";
-  allow: autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture;
-  loading: lazy;
-
-  @media (min-width: 768px) {
-    width: 60%;
-  }
-`;
+import "../styles/Streaming.css"; // Import the CSS file
 
 const Streaming = () => {
   return (
-    <Container>
-      <StyledIframe
+    <div className="streaming-container">
+      <iframe
+        className="styled-iframe"
         src="https://open.spotify.com/embed/artist/3SwSE7OtWzLOrc32Eq54gO?utm_source=generator&theme=0"
-      />
-    </Container>
+        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        loading="lazy"
+      ></iframe>
+    </div>
   );
 };
 

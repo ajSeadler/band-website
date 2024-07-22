0287052b44a20a89bdd715c12880c18dbf1c64ad
ajSeadler <aj6287@gmail.com>
2024-07-22 11:36:56 -0500

    fix grid on upcoming shows changed to flex)

--- src/components/MediaAndShowsGrid.jsx
+++ src/components/MediaAndShowsGrid.jsx
@@ -6,8 +6,12 @@ import "../styles/MediaAndShowsGrid.css";
 const MediaAndShowsGrid = () => {
   return (
     <div className="media-and-shows-grid">
-      <UpcomingShows />
-      <Media />
+      <div className="shows-section">
+        <UpcomingShows />
+      </div>
+      <div className="media-section">
+        <Media />
+      </div>
     </div>
   );
 };
